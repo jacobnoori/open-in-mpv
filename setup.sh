e42ca544780ccfe6e5ebda1ca8cf98d0b654f1d1
jacobnoori <jacob@jacobnoori.com>
2026-03-30 16:34:20 -0400

    Add directory creation step and support for Flatpak mpv

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@ set -ex
 # When the addon is installed or updated, there is one last thing to do.
 # To use the extension, run the following commands in your terminal:
 
+mkdir -p ~/.local/share/applications
+
 if [ -f mpv-scheme-handler.desktop ]; then
     cp -f mpv-scheme-handler.desktop ~/.local/share/applications/mpv-scheme-handler.desktop
 else
-    curl -sSf "https://raw.githubusercontent.com/binarynoise/open-in-mpv/main/mpv-scheme-handler.desktop" >~/.local/share/applications/mpv-scheme-handler.desktop
+    curl -sSf "https://raw.githubusercontent.com/jacobnoori/open-in-mpv/main/mpv-scheme-handler.desktop" >~/.local/share/applications/mpv-scheme-handler.desktop
 fi
-
-xdg-mime default mpv-scheme-handler.desktop x-scheme-handler/mpv
 
 set +ex
